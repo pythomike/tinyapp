33d57467dddbdf7926a8b2cba67ef47d550f7a3d
Mike <pythomike@gmail.com>
2017-10-11 15:41:57 +0000

    Added routing for POST request to /urls_new, function to create short URLs

--- express.js
+++ express.js
@@ -1,23 +1,49 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+const PORT = process.env.PORT || 8080; // default port 8080
+app.set('view engine', "ejs")
+
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function randomString () {
+  let shortURL = Math.random().toString(36).substring(2, 8)
+  return shortURL // RENAME THIS
+}
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
+app.get("/urls", (req, res) => {
+  let templateVars = {urls : urlDatabase}
+  res.render("urls_index", templateVars)
 })
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+})
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
